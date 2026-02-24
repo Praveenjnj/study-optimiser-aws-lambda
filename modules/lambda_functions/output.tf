4dcf9237ef04365eca60b67170339674607a248d
devopsdileepkb <dilee8217@gmail.com>
2026-02-24 14:04:54 +0530

    5

--- modules/lambda_functions/output.tf
+++ modules/lambda_functions/output.tf
@@ -1,7 +1,15 @@
 output "lambda_name" {
-  value = aws_lambda_function.zip_csv_lambda.function_name
+  value = (
+    lower(var.package_type) == "zip"
+    ? aws_lambda_function.zip_csv_lambda[0].function_name
+    : aws_lambda_function.image_lambda[0].function_name
+  )
 }
 
 output "lambda_arn" {
-  value = aws_lambda_function.zip_csv_lambda.arn
+  value = (
+    length(aws_lambda_function.zip_csv_lambda) > 0 ?
+    aws_lambda_function.zip_csv_lambda[0].arn :
+    aws_lambda_function.image_lambda[0].arn
+  )
 }
